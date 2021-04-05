1d9203d1c35d63645c0d42583ac74c577a46b9d2
Vuhiza <47759698+dmitriy337@users.noreply.github.com>
2021-04-05 11:58:20 +0300

    Create Config.cs

---
+++ TgBot/Config.cs
@@ -0,0 +1,8 @@
+namespace TgBot
+{
+    public struct Config
+    {
+        public static string TelegramToken = "YOUR TOKEN";
+        public static string ConnectionTobString = "YOUR CONNECTION TO DB STRING";
+    }
+}
