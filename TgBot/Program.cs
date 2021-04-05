824ec0cd57f4de30a54173788810f50272e01f58
ᐯㄩ卄丨乙卂 ᐯㄩ卄 <47759698+dmitriy337@users.noreply.github.com>
2021-04-05 11:25:56 +0300

    CreateProject

---
+++ TgBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TgBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
